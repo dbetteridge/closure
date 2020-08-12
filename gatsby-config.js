d1e40e8d434d7ba56df97e62e972015bc6eaaaf3
dbetteridge <danielrbetteridge@gmail.com>
2020-08-12 01:29:00 +0100

    Check with a folder that definitely exists

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,10 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages`,
+      },
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
